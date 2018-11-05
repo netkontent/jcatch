07aa312940aa38ef92ecc3c7707670037648b7c2
netkontent <biuro@netkontent.pl>
2018-11-05 18:59:05 +0000

    clean not needed code

--- module/routes.js
+++ module/routes.js
@@ -3,10 +3,10 @@ module.exports = function(root) {
   function init() {
 
     /* ##### START: ROUTES ##### */
-    root.app.get('/', _controller('home.js') );
-    root.app.get('/test', _controller('test.js') );
+    root.app.get('/', _controller('home') );
+    root.app.get('/test', _controller('test') );
 
-    // 404 - as last
+    // ignore /api/* 404 - as last
     root.app.use(/^\/(?!api).*/, (req, res) => {
       res.status('404').send('api - 404');
     } );
@@ -17,27 +17,21 @@ module.exports = function(root) {
 
   }
 
-  // private method
+  // controller autoloader
   function _controller( ctrl_name ) {
 
     const fs = require('fs');
     let ctrl_path = root._dir + '/controller/';
 
-    if( fs.existsSync( ctrl_path + ctrl_name ) ) {
+    if( fs.existsSync( ctrl_path + ctrl_name + '.js' ) ) {
 
-      return require( ctrl_path + ctrl_name );
+      return require( ctrl_path + ctrl_name + '.js' );
 
     } else {
 
       root.log('Controller [' + ctrl_name + '] is defined, but is not found.', 'warning');
 
-      return _missing_controller;
-    }
-
-
-    function _missing_controller( req, res, next ) {
-      root.log('Client tried access to not existing, but defined route: ' + req.url, 'error', {save: true});
-      return next(); // skip to 404
+      return next();
     }
 
   }
